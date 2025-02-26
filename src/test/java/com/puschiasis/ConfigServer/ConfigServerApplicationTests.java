d9e2cfe5105b990e95bfca27fd4e8e071023cb47
BrahianPuschiasis <brahian.puschiasis@gmail.com>
2025-02-25 22:46:45 -0300

    first commit

---
+++ src/test/java/com/puschiasis/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puschiasis.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
